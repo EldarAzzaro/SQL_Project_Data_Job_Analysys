47ecf2e3727a7812c228fd8ceac3c27f20a381ce
EldarAzzaro <61908243+EldarAzzaro@users.noreply.github.com>
2025-06-14 16:24:00 -0400

    top paying jobs

---
+++ project_sql/2_top_paying_job_skills.sql
@@ -0,0 +1,24 @@
+
+WITH top_paying_jobs AS(
+
+SELECT 
+job_id,
+job_title,
+name AS company_name,
+salary_year_avg
+FROM
+job_postings_fact
+LEFT JOIN 
+company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+job_title_short = 'Data Analyst' AND 
+job_location = 'Anywhere' AND
+salary_year_avg IS NOT NULL
+ORDER BY
+salary_year_avg DESC
+LIMIT 10;
+
+)
+
+
+
